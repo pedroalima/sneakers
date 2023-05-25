964a654a17f00c708ca75799ac1e386980f327bc
Pedro Lima <tsc.pedrolima@gmail.com>
2023-05-24 22:20:18 -0300

    first commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,24 @@
+import '../styles/components/navbar.sass';
+
+import iconMenu from '../images/icon-menu.svg';
+import iconLogo from '../images/logo.svg';
+import iconCart from '../images/icon-cart.svg';
+import imageUser from '../images/image-avatar.png';
+
+const Navbar = () => {
+
+    const topics = ["Collections", "Men", "Women", "About", "Contact"]
+    const topicsList = topics.map((topic, i) => <li key={"topic_" + i}><a href="#">{topic}</a></li>);
+
+    return (
+        <nav className="navbar">
+            <img className='menu' src={iconMenu} alt="Icon menu" />
+            <img className='logo' src={iconLogo} alt="logo" />
+            <ul>{topicsList}</ul>
+            <img className='cart' src={iconCart} alt="Icon cart" />
+            <img className='user' src={imageUser} alt="Image user" />
+        </nav>
+    )
+}
+
+export default Navbar
